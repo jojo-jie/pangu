06cffe10a178f63205f594b4cc5586489b3b2129
jojo-jie <18624275868@163.com>
2021-08-30 21:55:00 +0800

    docker-compose portainer

--- cmd/install.go
+++ cmd/install.go
@@ -13,7 +13,7 @@ var installCmd = &cobra.Command{
 	Short: "Docker installation and environment initialization",
 	Long:  "Docker installation and environment initialization",
 	Run: func(cmd *cobra.Command, args []string) {
-		shellCmds:=make([]*exec.Cmd, 0, 9)
+		shellCmds:=make([]*exec.Cmd, 0, 20)
 		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","sudo apt-get update"))
 		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release"))
 		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"))
@@ -23,6 +23,13 @@ var installCmd = &cobra.Command{
 		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","sudo systemctl status docker"))
 		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","q"))
 		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","sudo usermod -aG docker $USER"))
+		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","sudo curl -L \"https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose"))
+		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","sudo chmod +x /usr/local/bin/docker-compose"))
+		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","docker-compose --version"))
+		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","docker volume create portainer_data"))
+		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer"))
+		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","cd /usr/libexec/docker/"))
+		shellCmds = append(shellCmds, exec.Command("/bin/sh","-c","sudo ln -s docker-runc-current docker-runc"))
 		for _, shellCmd := range shellCmds {
 			fmt.Println("======******======")
 			fmt.Println(shellCmd.Args)
